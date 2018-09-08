b78a7b4784888304196ee10b59931ca67712518f
HalilCan <halilcanmemoglu@gmail.com>
2018-09-08 16:00:28 -0400

    SourceReader done

--- src/SourceReader.java
+++ src/SourceReader.java
@@ -1,4 +1,45 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SourceReader {
+    String csvPath;
+    String splitter = ",";
+    String lineBreak = "";
 
+    public SourceReader(String path) {
+        this.csvPath = "args[0]";
+
+    }
+
+    List<String> getSourceList() {
+        List<String> sourceList = new ArrayList<String>();
+        String line = "";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvPath))) {
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] links = line.split(splitter);
+
+                for (int i = 0; i < links.length; i++) {
+                    sourceList.add(links[i]);
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sourceList;
+    }
+
+    void setSplitter(String splitter) {
+        this.splitter = splitter;
+    }
+
+    void setLineBreak(String linebreak) {
+        this.lineBreak = linebreak;
+    }
 }
